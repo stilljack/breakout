f482db3b204f3c1d78b63b4f97c816036d0d73e9
jack seymour <stilljack@gmail.com>
2019-10-11 22:14:49 -0700

    and it just goes on like this --
    adding timber for reasons

--- app/src/main/java/com/saucefan/stuff/breakout/ui/main/MainFragment.kt
+++ app/src/main/java/com/saucefan/stuff/breakout/ui/main/MainFragment.kt
@@ -1,13 +1,13 @@
 package com.saucefan.stuff.breakout.ui.main
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
 import com.saucefan.stuff.breakout.R
 
 class BreakoutFragment : Fragment() {
@@ -35,16 +35,88 @@ class BreakoutFragment : Fragment() {
 
 
     class BreakoutView(context: Context) : SurfaceView(context),Runnable{
-        val gameThread = Thread()
+
+                  val gameThread = Thread()
+                  var mSurfaceHolder:SurfaceHolder =holder
+        @Volatile var playing:Boolean =false
+                  var paused = true
+                  var canvas =Canvas()
+                  val paint = Paint()
+                  var fps = 0L
+        private   var timeThisFrame=0L
+
+
 
 
 
         override fun run() {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            while (playing) {
+                //current time in milli
+                var startFrameTime = System.currentTimeMillis()
+
+                if (!paused) {
+                    update()
+                }
+
+                draw()
+
+                timeThisFrame = System.currentTimeMillis() - startFrameTime
+                if (timeThisFrame >= 1) {
+                    fps = 1000 / timeThisFrame
+                }
+            }
+        }
+
+        fun update() {
+
+        }
+
+        fun draw() {
+
+            if (mSurfaceHolder.surface.isValid) {
+                //lock the canvas
+                canvas =mSurfaceHolder.lockCanvas()
+
+                canvas.drawColor(resources.getColor(R.color.colorBackground))
+
+                paint.setColor(resources.getColor(R.color.colorBricks))
+
+                //draw a paddle
+
+                //draw ball
+
+                //draw bricks
+
+                //draw hud
+
+                //draw the rest
+
+                mSurfaceHolder.unlockCanvasAndPost(canvas)
+
+            }
+
+            fun pause(){
+                playing=false
+                try {
+                    gameThread.join()
+                } catch (e:InterruptedException) {
+                    Log.e("")
+                }
+            }
+
+
         }
 
 
+
+
+
+
     }
-
-
 }
+
+
+
+
+
+
